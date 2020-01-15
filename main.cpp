2894a17f2b48db21eaeeccc462cc2e457b92090d
jumorean <49334068+jumorean@users.noreply.github.com>
2020-01-15 11:56:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ Input input;
 
 
 
-void joint_states_callback(const sensor_msgs::JointStateConstPtr& joint_states_msg)
+void joint_states_callback(const sensor_msgs::JointStateConstPtr & joint_states_msg)
 {
 
     ROS_INFO("joint_space %f>>>>%d", joint_states_msg->position[0], i);
@@ -27,16 +27,11 @@ void joint_states_callback(const sensor_msgs::JointStateConstPtr& joint_states_m
     }    
 }
 
-void wb_ImuCallback(const nav_msgs::Odometry& tf_msg)
+void ImuCallback(const sensor_msgs::ImuConstPtr imu)
 {
-    q0 = tf_msg.pose.pose.orientation.w;
-    q1 = tf_msg.pose.pose.orientation.x;
-    q2 = tf_msg.pose.pose.orientation.y;
-    q3= tf_msg.pose.pose.orientation.z;
-    buffer[1] = 0-atan2(2 * (q2*q3+q0*q1),1-2*(q1*q1-q2*q2));
-    buffer[2] = 0-asin(-2*(q1*q3-q0*q2));
-    buffer[3] = atan2(2*(q1*q2+q0*q3),1-2*(q2*q2+q3*q3));
-    //ROS_INFO("RPY:%f %f %f",buffer[1]*180/3.14,buffer[2]*180/3.14,buffer[3]*180/3.14);
+    input.base_ang_vl = imu->angular_velocity;
+    input.base_lin_acc = imu->linear_acceleration;
+    input.base_quat = imu->orientation;
 }
 
 int main(int argc, char ** argv)
@@ -45,6 +40,9 @@ int main(int argc, char ** argv)
     ros::NodeHandle n;
     ros::Subscriber joint_states_sub = n.subscribe(
         "/pegasus2_model/joint_states", 1, joint_states_callback);
+    ros::Subscriber base_state_sub = n.subscribe(
+        "/pegasus2_model/imu", 1, ImuCallback
+    );
     //ros::Subscriber sub_lf_foot_bumper = n.subscribe(
       //  "/pegasus2_model/lf_foot_bumper", 1000, &lf_foot_bumper_callback);
     ros::spin();
